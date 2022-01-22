1e5891fa9ebcb189be43813737bcca00dab947f1
joeingjoe <joeingjoe3@gmail.com>
2022-01-22 15:40:03 +0800

    使用React-emotion完成基礎版面

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.css';
+import WeatherApp from './WeatherApp';
 import reportWebVitals from './reportWebVitals';
-
+    
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+        <WeatherApp />,
   document.getElementById('root')
 );
 
